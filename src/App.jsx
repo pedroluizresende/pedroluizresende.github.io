875b732c55c7f28ded5aabf95e5ba991a37cb915
Pedro Resende <pedroluizrnunez@gmail.com>
2023-03-28 23:04:47 -0300

    adciona linter ao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Home from './pages/Home'
-import { Route, Switch} from 'react-router-dom';
-import About from './pages/About'
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
- 
+
 function App() {
   return (
     <Switch>
