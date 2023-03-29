6c2209d2731ab147e24d6e480006dfc2a79434d0
Pedro Resende <pedroluizrnunez@gmail.com>
2023-03-28 22:49:00 -0300

    cria a rota de todas as páginas, Home, contato, sobre, e projetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-
+import Home from './pages/Home'
+import { Route, Switch} from 'react-router-dom';
+import About from './pages/About'
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+ 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio Pedro Resende 'teste'</h1>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/about" component={ About } />
+      <Route exact path="/contact" component={ Contact } />
+      <Route exact path="/projects" component={ Projects } />
+    </Switch>
   );
 }
-
 export default App;
