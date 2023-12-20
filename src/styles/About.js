80992ac31291178792dae17ed95a47b9eb02691f
Pedro Resende <pedroluizrnunez@gmail.com>
2023-12-20 13:36:59 -0300

    remove todos os arquivos

--- src/styles/About.js
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-import { Container } from '@mui/system';
-
-export const AboutContainer = styled(Container)`
-  background-color: ${({ theme }) => theme.palette.secondary.main};
-  height: 100vh;
-  width: 100%;
-`;
