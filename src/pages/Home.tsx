a9d88ee3227c6484bd31431b616b17cf6d659b97
Pedro Resende <pedroluizrnunez@gmail.com>
2024-01-08 10:18:27 -0300

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import styles from './Home.module.css';
+
+function Home() {
+  return (
+    <main className={ styles.container }>
+      <Header />
+    </main>
+  );
+}
+
+export default Home;
